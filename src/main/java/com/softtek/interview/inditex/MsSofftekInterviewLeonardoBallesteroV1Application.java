38cccdd8e5bb9aebf6b9bb616ad715ff69ebba8b
Leonardo Ballestero <ballesteroleonardojose@gmail.com>
2023-06-11 12:13:06 -0500

    chore: INT-001 first commit template

---
+++ src/main/java/com/softtek/interview/inditex/MsSofftekInterviewLeonardoBallesteroV1Application.java
@@ -0,0 +1,13 @@
+package com.softtek.interview.inditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSofftekInterviewLeonardoBallesteroV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSofftekInterviewLeonardoBallesteroV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/softtek/interview/inditex/MsSofftekInterviewLeonardoBallesteroV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softtek.interview.inditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSofftekInterviewLeonardoBallesteroV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
